ec421109e67e976a46b948581ba86f98cb0f5efa
sby051 <91495085+sby051@users.noreply.github.com>
2024-01-08 00:38:12 +0000

    chore: publish patch

---
+++ publish.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+pubtype=$1
+
+if [ -z "$pubtype" ]; then
+  echo "Usage: ./publish <major|minor|patch>"
+  exit 1
+fi
+
+if [ "$pubtype" != "major" ] && [ "$pubtype" != "minor" ] && [ "$pubtype" != "patch" ]; then
+  echo "Usage: ./publish <major|minor|patch>"
+  exit 1
+fi
+
+git add -A .
+git commit -m "chore: publish $pubtype"
+pnpm version $pubtype
+pnpm lint
+pnpm package
+pnpm publish
